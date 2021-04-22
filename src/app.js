75edb11ab7198f0376b89ca3519f46911a6580bc
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-22 13:38:05 +0000

    html listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var numRandom = Math.floor(Math.random() * (5 - 1)) + 1;
+  this.console.log(numRandom);
 };
